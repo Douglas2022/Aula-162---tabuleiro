da347fa505f1ecda8642edc874324093d26415d7
Douglas Silva <paulodnunes2016@gmail.com>
2024-01-16 10:33:21 -0300

    Peças no tabuleiro

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Xadrez2
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-           // tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPecas(new Torre(tab, Cor.Preta), new Posicao(1, 3));
             tab.ColocarPecas(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.ImprimirPeca(tab);
